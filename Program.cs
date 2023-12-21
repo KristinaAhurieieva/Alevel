e0c5d6367929ca19f0fd3a0d716ed2f3e33394f3
KristinaAhurieieva <90397467+KristinaAhurieieva@users.noreply.github.com>
2023-12-21 17:10:11 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] firstArray = new int[20];
+        int[] secondArray = new int[20];
+
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            firstArray[i] = new Random().Next(-1000, 1000);
+
+            if (firstArray[i] <= 888)
+            {
+                secondArray[i] = firstArray[i];
+            }
+
+        }
+
+
+        Array.Sort(secondArray) ;
+        Array.Reverse(secondArray);
+  
+
+        Console.WriteLine($"Values of first array: {string.Join(",", firstArray)}");
+
+        Console.WriteLine($"Values of second array: {string.Join(",", secondArray)}");
+
+    }
+
+}
+
