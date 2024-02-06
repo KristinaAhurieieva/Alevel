407fd5225ca016c98cd9e77f5006fc3253c4323e
Kristina Ahurieieva <kristi.komka97@gmail.com>
2024-02-06 13:44:06 +0400

    HomeWork 3.1

---
+++ HomeWork3.1/HomeWork3.1/Collection/PersonalisedCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using HomeWork3._1.Collection.Abstractions;
+
+namespace HomeWork3._1.Collection
+{
+    public class PersonalisedCollection<T> : System.Collections.ObjectModel.Collection<T>, IPersonalisedCollection<T>
+    {
+        private T[] collection = new T[0];
+
+        public int Count()
+        {
+            int counter = 0;
+            for (int idx = 0; idx < collection.Length; idx++)
+            {
+                if (collection[idx] != null)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public void Sort()
+        {
+            if (collection.Length > 0 && collection[0] is IComparable<T>)
+            {
+                Array.Sort(collection, (x, y) => ((IComparable<T>)x).CompareTo(y));
+            }
+            else
+            {
+                throw new InvalidOperationException("can't sort");
+            }
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref collection, collection.Length + 1);
+            collection[collection.Length - 1] = item;
+        }
+
+        public void SetDefaultAt(int index)
+        {
+            if (index >= 0 && index < collection.Length)
+            {
+                collection[index] = default(T);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "can't delete");
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return collection.Take(Count()).GetEnumerator();
+        }
+    }
+}
+
---
+++ HomeWork3.1/HomeWork3.1/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using HomeWork3._1.Collection;
+using HomeWork3._1.Collection.Abstractions;
+
+namespace HomeWork3._1
+{
+	public class Startup
+	{
+        private readonly IPersonalisedCollection<string> _personalisedCollection = new PersonalisedCollection<string>();
+
+
+        public void  Start()
+		{
+            PersonalisedCollection<string> personalisedCollection = new PersonalisedCollection<string>();
+
+            personalisedCollection.Add("Lily");
+            personalisedCollection.Add("Rose");
+            personalisedCollection.Add("Tulip");
+            personalisedCollection.Add("Daisy");
+            personalisedCollection.Add("Dandelion");
+            personalisedCollection.Add("Clover");
+
+
+            personalisedCollection.Sort();
+
+            Console.WriteLine("\nAfter sorting:");
+            foreach (var item in personalisedCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+            personalisedCollection.SetDefaultAt(2);
+
+            Console.WriteLine("\nAfter deleting at index 2:");
+            foreach (var item in personalisedCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+	}
+}
+
