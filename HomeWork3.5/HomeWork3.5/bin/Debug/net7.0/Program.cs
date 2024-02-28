baad3dc25ecdbd44b025128bc7f05bfeaf380810
Kristina Ahurieieva <kristi.komka97@gmail.com>
2024-02-28 15:17:28 +0400

    HomeWork3.5

---
+++ HomeWork3.5/HomeWork3.5/bin/Debug/net7.0/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            string result = await ReadDataFile("Hello", "World");
+            Console.WriteLine(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        Console.ReadLine();
+    }
+    static async Task<string> ReadDataFile(string fileone, string filenametwo)
+    {
+        Task<string> task1 = ReadFileAsync(fileone);
+        Task<string> task2 = ReadFileAsync(filenametwo);
+
+        await Task.WhenAll(task1, task2);
+
+        return task1.Result + task2.Result;
+    }
+
+    static async Task<string> ReadFileAsync(string filename)
+    {
+        using StreamReader reader = new StreamReader(filename);
+        return await reader.ReadToEndAsync();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
