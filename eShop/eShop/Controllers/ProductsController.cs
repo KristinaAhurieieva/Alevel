75ea627c30118c4e5b2614fb43953613d0506751
Kristina Ahurieieva <kristi.komka97@gmail.com>
2024-05-15 18:04:55 +0400

    HomeWork6.1

---
+++ eShop/eShop/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eShop.Models;
+
+namespace eShop.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController 
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Broccoli", "Carrot", "Pepper", "Potato", "Cucumber", "Tomato"
+    };
+
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetProduct")]
+    public IEnumerable<Product> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Product
+        {
+            DateAdded = DateTime.Now.AddDays(index), 
+            Grams = Random.Shared.Next(100, 500), 
+            Vegetable = Summaries[Random.Shared.Next(Summaries.Length)], 
+        })
+        .ToArray();
+
+    }
+
+    [HttpPost(Name = "PostProduct")]
+    public string Post(CalkProduct input)
+    {
+
+        return ProductServices.GetSummary(input.Product).ToString();
+        
+    }
+
+
+}
+
---
+++ eShop/eShop/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace eShop;
+
+
+public class Product
+{
+    public DateTime DateAdded { get; set; } 
+    public int Grams { get; set; } 
+    public string Vegetable { get; set; } 
+}
